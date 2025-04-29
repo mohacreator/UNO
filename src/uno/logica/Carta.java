635bfce077e736c726ee93af3005bc5fe01094e8
mohacreator <mmakhloufi964@boscdelacoma.cat>
2025-04-29 08:57:41 +0200

    Delete src directory

--- src/uno/logica/Carta.java
+++
@@ -1,9 +0,0 @@
-package uno.logica;
-
-
-public record Carta(int numero, uno.logica.Carta.Color color) {
-    public enum Color {
-        GROC, VERMELL, BLAU, VERD, Groc, Vermell, Blau, Verd
-    }
-
-}
